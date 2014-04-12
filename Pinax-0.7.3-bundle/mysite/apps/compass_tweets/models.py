b356de96f629aa68038490814cea38325f7a4793
amephraim <ame378@nyu.edu>
2014-04-11 22:21:34 -0400

    adding Pinax bundle, and mysite

---
+++ Pinax-0.7.3-bundle/mysite/apps/compass_tweets/models.py
@@ -0,0 +1,40 @@
+
+from django.db import models
+
+from microblogging.models import Tweet
+
+class Context(models.Model):
+    name = models.CharField(max_length=50)
+#    class Meta:
+#        app_label='test
+#    def __unicode__(self):
+#        return u"%s" % (self.name)    
+
+class Type(models.Model):
+    context = models.ForeignKey(Context)
+    name = models.CharField(max_length=50)
+#    class Meta:
+#        app_label='test'
+        
+class Role(models.Model):
+    context = models.ForeignKey(Context)
+    name = models.CharField(max_length=50)
+#    class Meta:
+#        app_label='test'
+
+class Rule(models.Model):
+    #original_tweet = models.OneToOneField(Tweet, primary_key=True)
+    message = models.CharField(max_length=80)
+    context = models.ForeignKey(Context)
+    message_type = models.ForeignKey(Type)
+    sender_role = models.ForeignKey(Role)
+    receiver_role = models.ForeignKey(Role)
+    bool_send = models.BooleanField(default=False)
+#    class Meta:
+#        app_label='test'
+# add field for tweet instances?
+
+
+    
+
+
