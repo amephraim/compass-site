994fb927c7a4de43cf7d67ac85ab5d4707b0e89b
amephraim <ame378@nyu.edu>
2014-04-23 13:46:58 -0400

    added placeholders s for newsfeed and context definition views

--- Pinax-0.7.3-bundle/mysite/compass_tweets/admin.py
+++ Pinax-0.7.3-bundle/mysite/compass_tweets/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from compass_tweets.models import Context,Type,Role,Rule
 
-admin.site.register(Context)
-admin.site.register(Type)
-admin.site.register(Role)
-admin.site.register(Rule)
+class TypeInline(admin.StackedInline):
+	model = Type
+	extra = 3
+	
+class RoleInline(admin.StackedInline):
+	model = Role
+	extra = 3
+	
+class RuleInline(admin.TabularInline):
+	model = Rule
+	extra = 2
+
+
+class ContextAdmin(admin.ModelAdmin):
+	fields = ['name']
+	inlines = [TypeInline,RoleInline,RuleInline]
+
+admin.site.register(Context,ContextAdmin)
