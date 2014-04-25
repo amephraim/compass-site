151b3f01535e4d1c4b2a77881572c45ace6d24b8
amephraim <ame378@nyu.edu>
2014-04-25 15:38:17 -0400

    fixed error in models.py

--- Pinax-0.7.3-bundle/mysite/compass_tweets/models.py
+++ Pinax-0.7.3-bundle/mysite/compass_tweets/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 #from microblogging.models import Tweet
-from check_constraints import Check, CheckConstraintMetaClass
+#from check_constraints import Check, CheckConstraintMetaClass
 
 class Context(models.Model):
     name = models.CharField(max_length=50)
@@ -27,7 +27,7 @@ class Role(models.Model):
 #        app_label='test'
 
 class Rule(models.Model):    
-    context = models.ForeignKey(Context,related_name='context')
+    context = models.ForeignKey(Context)
     message_type = models.ForeignKey(Type,related_name='message_type')
     sender_role = models.ForeignKey(Role, related_name='sender_name')
     receiver_role = models.ForeignKey(Role, related_name='receiver_name')
@@ -36,12 +36,12 @@ class Rule(models.Model):
 #        app_label='test'
 # add field for tweet instances?
 
-class improvedRules(models.Model):
-	context = models.ForeignKey(Context,related_name='context')
-	message_type = models.ForeignKey(Type,related_name='message_type')
-	sender_role = models.ForeignKey(Role, related_name='sender_name')
-	receiver_role = models.ForeignKey(Role, related_name='receiver_name')
-	bool_send = models.BooleanField(default=False)
+#lass improvedRules(models.Model):
+#	context = models.ForeignKey(Context)
+#	message_type = models.ForeignKey(Type,related_name='message_type')
+#	sender_role = models.ForeignKey(Role, related_name='sender_name')
+#	receiver_role = models.ForeignKey(Role, related_name='receiver_name')
+#	bool_send = models.BooleanField(default=False)
 
 class CTweet(models.Model):
 	message = models.CharField(max_length=500)
